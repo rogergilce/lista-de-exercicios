20ed525f7059fbac8525817cab8e6e0014944fc9
rogergilce <49981360+rogergilce@users.noreply.github.com>
2019-05-23 08:18:22 -0300

    Add files via upload

---
+++ prog023.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    int numero;
+    scanf("%d", &numero);   
+ if(numero%2 == 0)   
+ {       
+ printf("Par");   
+ }   
+ else   
+ {     
+  printf("Impar");   
+ }
+    return 0;
+}
